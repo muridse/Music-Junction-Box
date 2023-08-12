ecd7e7cff15475a11355ebe14f38460e87a334c6
Ilyas Karimov <mur1dse@outlook.com>
2023-08-12 13:16:51 +0300

    FileGrubber done

---
+++ Experimental/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Experimental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\Games\\Forza Horizon 5\\Content";
+            var arr = Directory.GetFiles(path);
+            foreach ( var item in arr ) 
+            {
+                //Console.WriteLine(item);
+            }
+
+            // Make a reference to a directory.
+            DirectoryInfo di = new DirectoryInfo(path);
+            // Get a reference to each file in that directory.
+            FileInfo[] fiArr = di.GetFiles();
+            var fiArrSorted = fiArr.OrderBy(p => p.Length);
+            foreach ( FileInfo fi in fiArrSorted ) 
+            {
+                Console.WriteLine($"{fi.ToString()} - {Math.Round((double)fi.Length / (1024 * 1024),2)} MB");
+            }
+        }
+    }
+}
--- Music Junction Box/Processors/FilesGrubber.cs
+++ Music Junction Box/Processors/FilesGrubber.cs
@@ -8,5 +8,34 @@ namespace Music_Junction_Box.Processors
 {
     internal class FilesGrubber
     {
+        private string InputPath;
+        private FileInfo[] Files;
+        public List<string> SortedPaths { get; private set; }
+        public FilesGrubber(string inputPath) 
+        {
+            InputPath = inputPath;
+            ScanDirectory();
+            FillSortedPaths();
+        }
+        private void ScanDirectory() 
+        {
+            DirectoryInfo di = new DirectoryInfo(InputPath);
+            Files = di.GetFiles();
+        }
+        private void FillSortedPaths() 
+        {
+            var sortedFiles = Files.OrderBy(f => f.Length);
+            foreach (FileInfo fi in sortedFiles) 
+            {
+                SortedPaths.Add(fi.ToString());
+            }
+        }
+        public void ShowFiles() 
+        {
+            foreach (var fi in Files.OrderBy(f=>f.Length))
+            {
+                Console.WriteLine($"{fi} - {Math.Round((double)fi.Length / (1024 * 1024), 2)} MB");
+            }
+        }
     }
 }
