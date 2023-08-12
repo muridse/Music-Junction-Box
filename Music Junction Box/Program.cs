8b50656759d22df121469572362284f9bf70bfd0
Ilyas Karimov <mur1dse@outlook.com>
2023-08-12 12:24:33 +0300

    Добавьте файлы проекта.

---
+++ Music Junction Box/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Music_Junction_Box
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
